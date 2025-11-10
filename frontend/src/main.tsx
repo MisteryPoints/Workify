2c0ec766dbc17723d9b15495d60d2a7632a57b85
MisteryPoints <misterypoint2@gmail.com>
2025-11-10 18:04:54 -0400

    Adding Login for Clerk

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Agregar la Publishable Key de Clerk desde las variables de entorno
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Agrega tu Publishable Key de Clerk a tu .env')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}> 
+      <App />
+    </ClerkProvider> 
   </StrictMode>,
 )
